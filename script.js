680cfa8b0287aa98c274d8deb4bb993aaedd4711
anis <anishammouche50@gmail.com>
2024-02-25 00:04:13 +0000

    addBooks

--- script.js
+++ script.js
@@ -1,21 +1,83 @@
+const modalContainer = document.querySelector('.modal-container');
+const modalTriggers=document.querySelectorAll(".modal-trigger");
 
-    function book(title,author, nbrPage, read) {
+modalTriggers.forEach( trigger => trigger.addEventListener('click', ()=>togglemodal()));
+
+function togglemodal() {
+    modalContainer.classList.toggle("active");    
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+class book{
+    constructor(title,author, nbrPage, read){
         this.title=title;
         this.author = author; 
         this.nbrPage=nbrPage;
         this.read=read;
-
-        this.consol=function () {
-            console.log("Title: "+this.title);
-            console.log( "Author :"+this.author);
-            if(this.nbrPage!=undefined){
-                console.log("Number of pages : "+this.nbrPage);
-            }
-            if(this.read != undefined) {
-                    console.log("Have you read it ? : "+this.read);
-            }
-         };    
-};
+    }
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+//  function book(title,author, nbrPage, read) {
             
-const bro=new book( "The Alchemist", "Paulo Coelho","294",true);
-bro.consol();
+//     this.title=title;
+//         this.author = author; 
+//         this.nbrPage=nbrPage;
+//         this.read=read;
+
+//         this.consol=function () {
+//             console.log("Title: "+this.title);
+//             console.log( "Author :"+this.author);
+//             if(this.nbrPage!=undefined){
+//                 console.log("Number of pages : "+this.nbrPage);
+//             }  
+//                     console.log("Have you read it ? : "+this.read);   
+//          };    
+// };
+const titre=document.querySelector("#titleInput");
+const author=document.querySelector("#author");
+const page=document.querySelector("#page");
+const read=document.querySelector("#check");
+const section=document.querySelector("section");
+let Books=[];
+
+const addBook=document.querySelector("#submit");
+addBook.addEventListener('click',() => setBook());
+
+
+function setBook() {
+let impposible=false;
+    Books.forEach(book=>{
+    if(titre.value===book.title) {
+        impposible=true ;
+    
+    }
+    });
+    if(!impposible){  Books.push( new book(titre.value,author.value,page.value,read.checked));
+    const clone=document.querySelector(`#bookTemplate`).content.cloneNode(true);
+        // let clone=temp.content.cloneNode(true);
+        console.log(clone);
+    clone.querySelector('.name').innerHTML=titre.value;
+    clone.querySelector('.autor').innerHTML=author.value;
+    clone.querySelector('.page').innerHTML=page.value;
+    section.appendChild(clone);
+    
+    }else{
+
+    }
+   
+    console.log(Books)
+}
+  
